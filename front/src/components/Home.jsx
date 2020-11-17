0b9937f5e70f747d4989a09b2ae071c3d191cef0
Robin <robin.dorion@gmail.com>
2020-11-17 15:57:05 +0100

    Set router and first comp, delete useless files in src

---
+++ front/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+	return (
+		<div>
+			<h4>Give a title to your event</h4>
+			<input type="text" />
+			<button>Next</button>
+		</div>
+	)
+}
+
+export default Home;
