939ebe0538d8d13a49e54638d99655ab22e4d864
Fritsch <v.fritsch1981@gmail.com>
2020-12-17 13:58:47 +0100

    Extends interface InputUser

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,68 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import User from './models/User';
 import { ApolloServer, gql } from 'apollo-server-express';
+import User from './models/User';
 
 const app = express();
 const port = 7777;
+
+const typeDefs = gql`
+  input InputUser {
+    firstname: String
+    lastname: String
+    email: String
+  }
+
+  type User {
+    _id: String
+    firstname: String
+    lastname: String
+    email: String
+  }
+
+  type Query {
+    getUser(email: String): User
+    getUsers: [User]
+  }
+
+  type Mutation {
+    addUser(user: InputUser): User
+  }
+`;
+interface InputUser {
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+
+const resolvers = {
+  Query: {
+    getUser: async (_: unknown, { email }: { email: string }) => {
+      // eslint-disable-next-line no-console
+      console.log('email', email);
+      const user = await User.findOne({ email });
+      return user;
+    },
+    getUsers: async () => User.find({}),
+  },
+
+  Mutation: {
+    addUser: async (args: InputUser) => {
+      try {
+        const user = new User(args);
+        const response = await user.save();
+        // eslint-disable-next-line no-console
+        console.log(response, args);
+        return response;
+      } catch (e) {
+        return e.message;
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 const start = async () => {
   try {
@@ -14,13 +72,16 @@ const start = async () => {
       useCreateIndex: true,
       autoIndex: true,
     });
+    // eslint-disable-next-line no-console
     console.log('Connected to database');
     app.listen(port, () =>
+      // eslint-disable-next-line no-console
       console.log(
         `Express GraphQL Server is now running on localhost:${port}${server.graphqlPath}`
       )
     );
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(err);
   }
 
@@ -33,54 +94,3 @@ const start = async () => {
   });
 };
 start();
-
-const typeDefs = gql`
-  input InputUser {
-    firstname: String
-    lastname: String
-    email: String
-  }
-
-  type User {
-    _id: ObjectId
-    firstname: String
-    lastname: String
-    email: String
-  }
-
-  type Query {
-    getUser(id: String): User
-    getUsers: [User]
-  }
-
-  type Mutation {
-    addUser(firstname: String!, lastname: String!, email: String!): User
-  }
-`;
-interface InputUser {
-  _id?: String;
-  firstname: String;
-  lastname: String;
-  email: String;
-}
-const resolvers = {
-  Query: {
-    getUser: async (id: string) => await User.findOne({ _id: id }).exec(),
-    getUsers: async () => await User.find({}).exec(),
-  },
-
-  Mutation: {
-    addUser: async (_: any, args: any) => {
-      try {
-        let response = await User.create(args);
-        console.log(response, args);
-        return response;
-      } catch (e) {
-        return e.message;
-      }
-    },
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app });
